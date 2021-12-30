7214ec3a014f990ee58f5e31a03ec3d34f6374ef
Thibault Grivel <thibaultgrivel1@gmail.com>
2021-12-30 15:19:00 +0100

    RealEstateManager

---
+++ app/src/androidTest/java/com/openclassrooms/realestatemanager/DaoTest.kt
@@ -0,0 +1,71 @@
+package com.openclassrooms.realestatemanager
+
+import androidx.room.Room
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import com.openclassrooms.realestatemanager.database.Database
+import com.openclassrooms.realestatemanager.model.Property
+import junit.framework.Assert.assertEquals
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class DaoTest {
+
+    private lateinit var mDatabase: Database
+    private var mProperty = Property()
+
+    @Before
+    @Throws(Exception::class)
+    fun initDataBase() {
+        mDatabase = Room.inMemoryDatabaseBuilder(InstrumentationRegistry.getInstrumentation().context,Database::class.java)
+                .allowMainThreadQueries()
+                .build()
+    }
+
+    @After
+    @Throws(Exception::class)
+    fun closeDatabase(){
+        mDatabase.close()
+    }
+
+    @Test
+    @Throws(InterruptedException::class)
+    fun getProperties() {
+        val properties = this.mDatabase.propertyDao.getProperty().value
+        if (properties != null) {
+            //Database is prepopulate with 2 property
+            assertEquals(2, properties.size)
+        }
+    }
+
+    @Test
+    @Throws(InterruptedException::class)
+    fun insertProperty() {
+        this.mDatabase.propertyDao.insertProperty(mProperty)
+        val properties = this.mDatabase.propertyDao.getProperty().value
+        if (properties != null) {
+            assertEquals(3, properties.size)
+        }
+    }
+
+    @Test
+    fun updateProperty() {
+        mProperty = Property(mProperty.id, "I have been updated with success",0
+                ,0,0,"","","","","",
+                "","","")
+        this.mDatabase.propertyDao.updateProperty(mProperty)
+        assertEquals("I have been updated with success", mProperty.type)
+    }
+
+    @Test
+    fun deleteProperty() {
+        this.mDatabase.propertyDao.deleteProperty(mProperty.id)
+        val properties = this.mDatabase.propertyDao.getProperty().value
+        if (properties != null) {
+            assertEquals(3, properties.size)
+        }
+    }
+}
--- app/src/test/java/com/openclassrooms/realestatemanager/UtilsUnitTest.java
+++ app/src/test/java/com/openclassrooms/realestatemanager/UtilsUnitTest.java
@@ -2,32 +2,28 @@ package com.openclassrooms.realestatemanager;
 
 import org.junit.Test;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class UtilsUnitTest {
 
     private final String nowDate = new SimpleDateFormat("dd/MM/yyyy").format(new Date());
 
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
-    }
-
-    @Test
-    public void eurosToDollars(){
+    public void eurosToDollars() {
         assertEquals(Utils.convertEuroToDollar(5000), 5710);
     }
 
     @Test
-    public void dollarsToEuros(){
+    public void dollarsToEuros() {
         assertEquals(Utils.convertDollarToEuro(780000), 633360);
     }
 
@@ -35,4 +31,11 @@ public class ExampleUnitTest {
     public void getTodayDate() {
         assertEquals(nowDate, Utils.getTodayDate());
     }
+
+    @Test
+    public void transformDate() throws ParseException {
+        SimpleDateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        assertEquals(format.parse(nowDate), Utils.stringToDate(nowDate));
+    }
+
 }
